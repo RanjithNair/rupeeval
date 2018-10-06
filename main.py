51d20e78ecda4e674e8ddf67c1985e85f165870d
RanjithNair <ranjith2112@gmail.com>
2018-10-05 20:52:54 -0500

    Fixed Remitly issues

--- main.py
+++ main.py
@@ -10,10 +10,23 @@ def westernunion():
     print(exchRate)
 
 def xoom():
+    print("****** XOOM ******")
     browser = mechanicalsoup.StatefulBrowser()
     browser.open("https://www.xoom.com/india/send-money")    
     page = browser.get_current_page()
     messages = page.find("div", class_="js-exchange-rate")
     if messages:
         print(messages.text)
+
+
+def remitly():
+    print("****** REMITLY ******")
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open("https://www.remitly.com/us/en/india/pricing")
+    page = browser.get_current_page()
+    rates = page.find("div", class_="f1jkwmje fawncrr")
+    if rates:
+        print(rates.text)
+
 xoom()
+remitly()
